f12a56b2a9eb8ef7869f38318009278313335eae
Struckdown <bfleysher@gmail.com>
2020-05-08 19:22:59 -0600

    Set up basic pellets and world

--- src/Scripts/MouseManager.gd
+++ src/Scripts/MouseManager.gd
@@ -7,11 +7,22 @@ enum {SELLING, FEED_BASIC, FEED_SPECIAL}
 
 
 func _input(event):
-   # Mouse in viewport coordinates
-   if event is InputEventMouseButton:
-	   print("Mouse Click/Unclick at: ", event.position)
+	# Mouse in viewport coordinates
+	if event is InputEventMouseButton:
+		print("Mouse Click/Unclick at: ", event.position)
+		#IF VALID POSITION #TODO
+		spawnPellet(event.position)
+
    #elif event is InputEventMouseMotion:
 	#   print("Mouse Motion at: ", event.position)
 
    # Print the size of the viewport
    #print("Viewport Resolution is: ", get_viewport_rect().size)
+
+
+func spawnPellet(location):
+	var scene = load("res://Scenes/Pellet.tscn")
+	var pellet = scene.instance()
+	get_parent().add_child(pellet)
+	pellet.position = location
+	print("Pellet spawned")
---
+++ src/Scripts/Pellet.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var FALL_SPEED = 1
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	move()
+	
+
+func move():
+	self.position.y += FALL_SPEED
