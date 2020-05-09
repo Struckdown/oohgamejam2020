bd5af0fb6c25236b245dee3048e5b5927ca27c9e
Struckdown <bfleysher@gmail.com>
2020-05-09 13:32:46 -0600

    Added support for pellet types

--- src/Scripts/Pellet.gd
+++ src/Scripts/Pellet.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
 var FALL_SPEED = 1
-
+enum pelletTypeOptions {PINK, YELLOW, GRAY}
+var pelletType
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AudioStreamPlayer2D.play(0)
 	$Sprite.frame = randi() % 6
 	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -17,3 +17,8 @@ func _process(_delta):
 
 func move():
 	self.position.y += FALL_SPEED
+
+
+func setPelletType(type):
+	pelletType = type
+	#TODO update graphic
