3c99c1135ee2f25727b6b54ce1effc6ff1a8a862
Struckdown <bfleysher@gmail.com>
2020-05-09 13:34:37 -0600

    Updated pellet type options

--- src/Scripts/Pellet.gd
+++ src/Scripts/Pellet.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var FALL_SPEED = 1
-enum pelletTypeOptions {PINK, YELLOW, GRAY}
+enum pelletTypeOptions {PINK, GREEN, YELLOW, BLUE}
 var pelletType
 
 # Called when the node enters the scene tree for the first time.
