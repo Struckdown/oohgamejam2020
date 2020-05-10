acb89cd1a80ae46775c48e16405ad27970d7b5e5
Struckdown <bfleysher@gmail.com>
2020-05-10 13:10:30 -0600

    Fish dex almost done

--- src/Scripts/FishDex.gd
+++ src/Scripts/FishDex.gd
@@ -3,11 +3,13 @@ extends Control
 
 var currentPage = 0
 var maxPages = 5
-
+var pages = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var children = $Master/Content.get_children()
+	for child in children:
+		pages.append(child)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,13 +23,16 @@ func toggleVis():
 		show()
 
 func changePage(newPage):
-	if newPage > 0 and newPage < maxPages:	
+	if newPage >= 0 and newPage < len(pages):
 		currentPage = newPage
+	for p in pages:
+		p.hide()
+	pages[currentPage].show()
 
 
-func _on_ForwardButton_toggled(_button_pressed):
+func _on_ForwardButton_pressed():
 	changePage(currentPage+1)
 
 
-func _on_BackwardsButton_toggled(_button_pressed):
+func _on_BackwardsButton_pressed():
 	changePage(currentPage-1)
